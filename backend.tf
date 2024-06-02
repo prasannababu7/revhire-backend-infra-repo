18e48ff45ac16b1fbab7d0229b9821d87b360b61
Prasanna Babu <prasannababuvijjana@gmail.com>
2024-06-02 15:19:29 +0530

    aded

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-backend-for-revhire-infrastructure-creation"
+    bucket         = "s3-revhire-backend-bucket"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "revhire-backend-table"
+    dynamodb_table = "revhire-dynamodb-backend-infra"
   }
 }
 
